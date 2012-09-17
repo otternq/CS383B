daa2f6672b8940062a770360151934e61d63c9bd
Nick Otter <otternq@gmail.com>
2012-09-17 21:48:13 +0000

    updated the example python file that connects to a MongoDB instance to show all items in a collection

--- stats/connect.py
+++ stats/connect.py
@@ -1,14 +1,17 @@
-from pymongo import Connection
+from pymongo import Connection #Connection makes a connection to a MongoDB instance
 
-DATABASE_HOME = "ds037407-a.mongolab.com"
-DATABASE_PORT = 37407
-DATABASE_NAME = "socialstock"
+DATABASE_HOME = "ds037407-a.mongolab.com" #the host IP address
+DATABASE_PORT = 37407 #the port to connect to
+DATABASE_NAME = "socialstock" #the database the file is working with
+DATABASE_USERNAME = "otternq" #the username the file is working with
+DATABASE_PASSWORD = "Swimm3r." #the password the file is working with
 
-connection = Connection(DATABASE_HOME, DATABASE_PORT);
+connection = Connection(DATABASE_HOME, DATABASE_PORT); #connect to the MongoDB instance
 
-db = connection[DATABASE_NAME]
-db.authenticate("otternq", "Swimm3r.")
+db = connection[DATABASE_NAME] #specify the database to use
+db.authenticate(DATABASE_USERNAME, DATABASE_PASSWORD) #authenticate to the database
 
-messages = db.messages
+messages = db.messages #specify the collection to use
 
-print messages.find_one()
+for message in messages.find(): #do the following for each item in the collection
+	print message #print the item
