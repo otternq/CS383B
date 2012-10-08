d0463c3beaee2b46d285c8b6d0400370a43e1c25
pina3608 <pina3608@vandals.uidaho.edu>
2012-10-07 22:56:00 -0700

    Facebook test

--- PHPUnitTest/socialTest.php
+++ PHPUnitTest/socialTest.php
@@ -199,7 +199,15 @@ class SocialServiceTest extends PHPUnit_Framework_TestCase {
     
     
     public function subTestRetrieveMessagesFacebook( $message ) {
-	$test = "Hello";
+        
+        $data = json_decode( $message );
+        
+        // (expected, actual)
+        $this->assertInternalType( "array", $data->results );
+
+        // (expected, actual) may not be a good test, what if the response is
+        // good but there were no result entries
+        $this->assertGreaterThan( 0, count( $data->results ) );
 	}
     public function subTestRetrieveMessagesReddit( $message ) {}
     
