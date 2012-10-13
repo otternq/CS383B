bda27db4a4deffc77b147423de5d34d83bf696a8
Nick Otter <otternq@gmail.com>
2012-10-12 18:40:56 -0700

    Adding functionality for a financial class that uses Yahoo for data

---
+++ getSocialData/financial/stockEntry.php
@@ -0,0 +1,12 @@
+<?php
+
+class StockEntry {
+    public $open = null;
+    public $close = null;
+    public $high = null;
+    public $low = null;
+    public $volume = null;
+    public $date = null;
+}
+
+?>
