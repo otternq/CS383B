16e8869a58459a0bae86b57cabcc535e657b94e6
pina3608 <pina3608@vandals.uidaho.edu>
2012-11-09 16:41:28 -0800

    Gauge should be working

--- api/guage.php
+++ api/guage.php
@@ -2,7 +2,42 @@
 header('content-type: application/json; charset=utf-8');
 header("access-control-allow-origin: *");
 
-$json = json_encode(array("a1"=>1, "a2" => 2, "a3" => 3));
+
+/* Where to go, which database, and which collection */
+$m = new Mongo("mongodb://otternq:Swimm3r.@ds037407.mongolab.com:37407/socialstock");
+$db = $m->selectDB('socialstock');
+$collection = new MongoCollection($db, 'results');
+
+/* Create an array for the sentiment */
+$sentArr = array();
+
+for($algorithm = 1; $algorithm <=3; $algorithm++){
+
+  /* Process the information into an array */
+  foreach($collection->find(array("algorithm" => $algorithm)) as $message){
+    /* Make the indexes variables */
+    $firstIndex = date("Y-m-d", $message['date']);
+
+    /* load the result into the array */
+    if ( !isset($sentArr[$algorithm][$firstIndex]) ) $sentArr[$firstIndex] = $message['result'];
+    else $sentArr[$algorithm][$firstIndex] += $message['result'];
+
+  }
+}
+
+			/* Alphabetize the entries */
+for($algorithm = 1; $algorithm <=3; $algorithm++){
+
+  foreach( array_keys($sentArr[$algorithm]) as $arrKey)  {
+
+    ksort($sentArr[$algorithm][$arrKey]);
+
+  }
+}
+
+
+$json = json_encode(array("a1"=> array_pop($sentArr[1]),
+                "a2" => array_pop($sentArr[2]), "a3" => array_pop($sentArr[3])));
 
 echo isset($_GET['callback'])
     ? "{$_GET['callback']}($json)"
